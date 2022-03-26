3b1cc9cbe27de396c18b0e827ee3bdb6a1f29ba3
effiets <efoula86@gmail.com>
2022-03-26 12:41:16 +0200

    make website responsive

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ let paint = document.getElementById("paint")
 //profile pic
 
 avatar.onclick = function(){
-    if(avatar.src.match("./resources/mea.png")) {
-        avatar.src = "./resources/meb.png";
+    if(avatar.src.match("resources/mea.png")) {
+        avatar.src = "resources/meb.png";
     } else {
-        avatar.src="./resources/mea.png";
+        avatar.src="resources/mea.png";
     }
     
     avatar.style.cursor="pointer"
@@ -19,10 +19,10 @@ avatar.onclick = function(){
 
 //books pic
 books.onclick = function(){
-if(books.src.match("./resources/booka.png")){
-    books.src = "./resources/bookb.png"
+if(books.src.match("resources/booka.png")){
+    books.src = "resources/bookb.png"
 } else {
-    books.src="./resources/booka.png"
+    books.src="resources/booka.png"
 }
 
 books.style.cursor="pointer"
@@ -30,19 +30,19 @@ books.style.cursor="pointer"
 
 //dog pic
 dog.onclick = function(){
-    if(dog.src.match("./resources/doga.png")){
-        dog.src = "./resources/dogb.png"
+    if(dog.src.match("resources/doga.png")){
+        dog.src = "resources/dogb.png"
 } else {
-    dog.src="./resources/doga.png"
+    dog.src="resources/doga.png"
 }
     dog.style.cursor="pointer"
 }
 
 game.onclick = function(){
-    if(game.src.match("./resources/gamea.png")){
-        game.src = "./resources/gameb.png"
+    if(game.src.match("resources/gamea.png")){
+        game.src = "resources/gameb.png"
 } else {
-    game.src="./resources/gamea.png"
+    game.src="resources/gamea.png"
 }
 
 game.style.cursor="pointer"
@@ -50,10 +50,10 @@ game.style.cursor="pointer"
 
 //draw pic
 paint.onclick = function(){
-    if(paint.src.match("./resources/drawa.png")){
-        paint.src = "./resources/drawb.png"
+    if(paint.src.match("resources/drawa.png")){
+        paint.src = "resources/drawb.png"
 } else {
-    paint.src="./resources/drawa.png"
+    paint.src="resources/drawa.png"
 }
 
 paint.style.cursor="pointer"
